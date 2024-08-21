d9f825da9b224abb91ddc1d0af410ef0d081bb4d
Nripendra Kumar <strcse16@gmail.com>
2024-08-21 12:05:05 +0530

    StringCalculator

---
+++ src/main/tdd/StringCalculator.java
@@ -0,0 +1,51 @@
+public class StringCalculator {
+
+    public int add(String numbers) {
+        if (numbers.length() < 2) {
+            if (numbers.isEmpty()) {
+                return 0;
+            } else {
+                return convertToInt(numbers);
+            }
+        } else {
+            String delimiter = ",";
+            if (numbers.matches("//(.*)\n(.*)")) {
+                delimiter = Character.toString(numbers.charAt(2));
+                numbers = numbers.substring(4);
+            }
+
+            String[] numList = splitNumbers(numbers, delimiter + "|\n");
+            return sum(numList);
+        }
+    }
+
+    public String[] splitNumbers(String numbers, String divider) {
+        return numbers.split(divider);
+    }
+
+    public int convertToInt(String num) {
+        return Integer.parseInt(num);
+    }
+
+    public int sum(String[] numbers) {
+        int total = 0;
+        StringBuilder negativeString = new StringBuilder();
+
+        for (String number : numbers) {
+            if (convertToInt(number) < 0) {
+                if (negativeString.toString().equals(""))
+                    negativeString = new StringBuilder(number);
+                else
+                    negativeString.append(",").append(number);
+            }
+            if (convertToInt(number) < 1000)
+                total += convertToInt(number);
+        }
+
+        if (!negativeString.toString().equals("")) {
+            throw new IllegalArgumentException("Negatives not allowed: " + negativeString);
+        }
+
+        return total;
+    }
+}
